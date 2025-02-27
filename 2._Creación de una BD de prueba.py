be58ae8e274b2fe3848897af2b0d109e7af74e8f
Bit4ciD <bitacid@gmail.com>
2025-02-27 19:02:51 +0100

    1

---
+++ 2._Creación de una BD de prueba.py
@@ -0,0 +1,22 @@
+# En este ejemplo se crea una BD en el servidor 
+
+import mysql.connector
+
+conn1 = mysql.connector.connect(host="localhost",user="root",passwd="")
+                               
+cur1 = conn1.cursor()
+
+# Creamos la BD y comprobamos
+
+cur1.execute("CREATE DATABASE basedepruebas")
+cur1.execute("show databases")
+for base in cur1:
+   print(base)
+
+# Borramos la BD y comprobamos
+
+cur1.execute("DROP DATABASE basedepruebas")
+cur1.execute("show databases")
+for base in cur1:
+    print(base)
+conn1.close()
