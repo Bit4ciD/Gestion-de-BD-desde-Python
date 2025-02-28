d15402605157e5efed37f68854eb0166c4116cb5
Bit4ciD <bitacid@gmail.com>
2025-02-28 22:53:19 +0100

    1

--- 4._Crear una contraseña para la BD.py
+++ 4._Crear una contraseña para la BD.py
@@ -5,11 +5,14 @@ conn1 = mysql.connector.connect(host="localhost",user="root",passwd="")
 cur1 = conn1.cursor()
 
 #CAMBIAMOS LA CONTRASEÑA DEL USUARIO
-cur1.execute("SET PASSWORD FOR 'root'@'localhost'='1234PERIQUITO$'")
+cur1.execute("SET PASSWORD FOR 'root'@'localhost'='1234datos$'")
 
 #ESPERAMOS ACCEDER Y VER EL LISTADO DE BD
 
 cur1.execute("show databases")
+print("Esta es la lista de las bases de datos existentes")
+num = 0
 for base in cur1:
-    print(base)
+    num = num + 1
+    print("Elemento ", num, base)
 conn1.close()
